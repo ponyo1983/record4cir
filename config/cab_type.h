8130f222b46d202e4c15a26b64424ba4129b78bb
lifeng <coding4fun@126.com>
2016-03-28 15:39:04 +0800

    (1)加入机车型号配置

---
+++ config/cab_type.h
@@ -0,0 +1,15 @@
+/*
+ * cab_type.h
+ *
+ *  Created on: Mar 28, 2016
+ *      Author: lifeng
+ */
+
+#ifndef CONFIG_CAB_TYPE_H_
+#define CONFIG_CAB_TYPE_H_
+
+
+char * get_cab_code(int id);
+
+
+#endif /* CONFIG_CAB_TYPE_H_ */
